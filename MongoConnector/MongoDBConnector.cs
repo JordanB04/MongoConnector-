805d7e587c11737758c8dcfe39d0807944243c65
Jordan Broomfield <Jordan.Broomfield2004@yahoo.com>
2025-12-01 18:23:40 -0500

    Feature: Add InsertDataAsync and GetDataAsync methods

--- MongoConnector/MongoDBConnector.cs
+++ MongoConnector/MongoDBConnector.cs
@@ -27,4 +27,17 @@ public class MongoDBConnector
     }
 }
 
+public async Task InsertDataAsync(string collectionName, MyData data)
+{
+    var collection = _database.GetCollection<MyData>(collectionName);
+    await collection.InsertOneAsync(data);
+}
+
+public async Task<MyData?> GetDataAsync(string collectionName, int id)
+{
+    var collection = _database.GetCollection<MyData>(collectionName);
+    return await collection.Find(x => x.Id == id).FirstOrDefaultAsync();
+}
+
+
 }
---
+++ MongoConnector/MyData.cs
@@ -0,0 +1,7 @@
+namespace MongoConnector;
+
+public class MyData
+{
+    public int Id { get; set; }
+    public string Value { get; set; } = "";
+}
