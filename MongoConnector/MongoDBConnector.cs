a765e1d5aca33500b5d8f796e5c76da1fd00af69
Jordan Broomfield <Jordan.Broomfield2004@yahoo.com>
2025-12-01 18:15:55 -0500

    Initial Commit

---
+++ MongoConnector/MongoDBConnector.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Driver;
+
+namespace MongoConnector;
+
+public class MongoDBConnector
+{
+    private readonly MongoClient _client;
+    private readonly IMongoDatabase _database;
+
+    public MongoDBConnector(string connectionString)
+    {
+        _client = new MongoClient(connectionString);
+        _database = _client.GetDatabase("TestDB");
+    }
+
+    public bool Ping()
+    {
+        try
+        {
+            var command = new BsonDocument("ping", 1);
+            _database.RunCommand<BsonDocument>(command);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
